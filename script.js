72425bc8ba608c33a07d5b0fd023d0b571f8956d
ValouZ <debray.va@gmail.com>
2020-12-25 01:25:33 +0100

    bug corrections where 2 timers where running at the same time

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const bill = document.getElementById("app-bill");
 const payslip = document.getElementById("app-payslip");
 const fees = document.getElementById("app-fees");
 let indexRow = 0;
+let intervalStatut = false;
 
 // Elements used to make elements appears
 let y;
@@ -50,50 +51,62 @@ if (document.documentElement.clientWidth >= 1440) {
 
 // --------------------FUNCTIONS--------------------
 
-// Set a class to the clicked button to sho it and call the right API.
+// Function that calls the right API.
 function selected(e) {
     e.preventDefault();
-    for (let j = 0; j < buttons.length; j++) {
-        buttons[j].classList.remove("selection");
-    }
-    this.classList.add("selection");
     if (this == bill) {
-        displaySelected('https://secure.askolga.fr/bucket/b744b24d0faad9334a0ecfe777550eceda50c707dd0914ae86a7c8bc15456c3d5fe1f47cb471e/search/08ce4f9d55489fa458bd1b2e1ce43b445c77ce571f06491ff99dfb19b9fa97005fe1f5043359a-show.json');
+        displaySelected('https://secure.askolga.fr/bucket/b744b24d0faad9334a0ecfe777550eceda50c707dd0914ae86a7c8bc15456c3d5fe1f47cb471e/search/08ce4f9d55489fa458bd1b2e1ce43b445c77ce571f06491ff99dfb19b9fa97005fe1f5043359a-show.json', this);
     } else if (this == payslip) {
-        displaySelected('https://secure.askolga.fr/bucket/b744b24d0faad9334a0ecfe777550eceda50c707dd0914ae86a7c8bc15456c3d5fe1f47cb471e/search/c53f67111d90e53b638841ae63edc145e833cbb64bada4e6ad6ac660474c64c65fe1f53a5f913-show.json');
+        displaySelected('https://secure.askolga.fr/bucket/b744b24d0faad9334a0ecfe777550eceda50c707dd0914ae86a7c8bc15456c3d5fe1f47cb471e/search/c53f67111d90e53b638841ae63edc145e833cbb64bada4e6ad6ac660474c64c65fe1f53a5f913-show.json', this);
     } else {
-        displaySelected('https://secure.askolga.fr/bucket/b744b24d0faad9334a0ecfe777550eceda50c707dd0914ae86a7c8bc15456c3d5fe1f47cb471e/search/e36ea1889b3687e8357f4796ad7d17cce046b93b104bce8e61e8d1ad194fdc725fe1f4e350766-show.json');
+        displaySelected('https://secure.askolga.fr/bucket/b744b24d0faad9334a0ecfe777550eceda50c707dd0914ae86a7c8bc15456c3d5fe1f47cb471e/search/e36ea1889b3687e8357f4796ad7d17cce046b93b104bce8e61e8d1ad194fdc725fe1f4e350766-show.json', this);
     }
 }
 
-// Function displaying the content of the called url
-function displaySelected(url) {
-    elements.innerHTML = "";
-    let compteur = 0;
-    fetch(url)
-        .then(response => response.json())
-        .then(function(data) {
-            let doc = data.documents;
-            for (let i in doc) {
-                if (compteur % 2 == 0) {
-                    elements.innerHTML += "<div class='left-row row dark'>" + doc[i].filename + "</div>";
-                } else {
-                    elements.innerHTML += "<div class='right-row row normal'>" + doc[i].filename + "</div>";
-                }
-                compteur++;
-            }
-            indexRow = 0;
-            let interval = setInterval(() => { displayRow(interval) }, 100);
+function setSelectionClass(myButton){
+    for (let j = 0; j < buttons.length; j++) {
+        buttons[j].classList.remove("selection");
+    }
+    myButton.classList.add("selection");
+}
 
-            if (compteur < 2)
-                countResults.textContent = compteur + " document trouvé";
-            else
-                countResults.textContent = compteur + " documents trouvés";
-        });
+// Function displaying the content of the called url
+function displaySelected(url, myButton) {
+    // If the intervalStatut, it means that an interval is running and
+    // we should not launch an other one yet
+    if (!intervalStatut) {
+        setSelectionClass(myButton);
+        intervalStatut = true;
+        elements.innerHTML = "";
+        let compteur = 0;
+        fetch(url)
+            .then(response => response.json())
+            .then(function(data) {
+                let doc = data.documents;
+                for (let i in doc) {
+                    if (compteur % 2 == 0) {
+                        elements.innerHTML += "<div class='left-row row dark'>" + doc[i].filename + "</div>";
+                    } else {
+                        elements.innerHTML += "<div class='right-row row normal'>" + doc[i].filename + "</div>";
+                    }
+                    compteur++;
+                }
+                indexRow = 0;
+                let interval = setInterval(() => { displayRow(interval) }, 100);
+
+                if (compteur <= 0)
+                    countResults.textContent = "Aucun document trouvé";
+                else if (compteur == 1)
+                    countResults.textContent = compteur + " document trouvé";
+                else
+                    countResults.textContent = compteur + " documents trouvés";
+            });
+    }
 }
 
 function displayRow(interval) {
     if (indexRow == elements.children.length - 1) {
+        intervalStatut = false;
         clearInterval(interval);
     }
     if (indexRow % 2 == 0)
